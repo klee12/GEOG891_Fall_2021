59cf997ef0bed28d0dd928b4ab474dd11948003c
Kun-Yuan Lee <jasonklee@huskers.unl.edu>
2021-09-23 00:24:01 -0500

    Lab1_Final_Lee
    GEOG891_Lab 1

--- homework1/lab1work.R
+++ homework1/lab1work.R
@@ -10,8 +10,8 @@ d.stations <- sf::read_sf(p.stations)
 
 
 #1.1
-d.counties %>% select(NAME10,ALAND10,AWATER10) %>% mutate(landAreaPrec = ALAND10/sum(ALAND10+AWATER10)*100)
-  
+d.counties %>% as_tibble() %>% dplyr::select(NAME10,ALAND10,AWATER10) %>% mutate(landAreaPrec = ALAND10/sum(ALAND10+AWATER10)*100)%>%
+  dplyr::select(NAME10,landAreaPrec) 
 
 
 
@@ -20,23 +20,20 @@ d.counties %>% select(NAME10,ALAND10,AWATER10) %>% mutate(landAreaPrec = ALAND10
 d.counties %>% as_tibble() %>% dplyr::select(-geometry) %>%
   group_by(STATEFP10) %>% select(NAME10,ALAND10,AWATER10) %>% 
   mutate(LargPLandArea=max(AWATER10/sum(ALAND10+AWATER10))) %>% 
-  slice(which.max(LargPLandArea))
+  slice(which.max(LargPLandArea)) %>% dplyr::select(STATEFP10,NAME10,LargPLandArea) 
   
 
 #1.3
-d.counties %>% as_tibble() %>% dplyr::select(-geometry) %>%
-  group_by(STATEFP10) %>% count(NAME10,STATEFP10)
 
-d.counties %>% as_tibble() %>% dplyr::select(-geometry) %>% count(STATEFP10,sort = TRUE)
+d.counties %>% as_tibble() %>%count(STATEFP10,sort = TRUE)
 
 #1.4
-d.stations %>% as_tibble() %>% dplyr::select(-geometry) %>% length(STATION_NA)
 
 which(nchar(d.stations$STATION_NA)==min(nchar(d.stations$STATION_NA))) %>%d.stations[.,]%>% dplyr::select(STATION_NA)
 
 #2.1 
 d.counties %>% ggplot(., aes(x= ALAND10,y=AWATER10)) +
-  geom_point(aes(col=COUNTYFP10))+
+  geom_point(aes(col=STATEFP10))+
   labs(title = "Land area and Water area for each County")+
   xlab("Land Area") + 
   ylab("Water Area")
@@ -58,48 +55,18 @@ d.stations %>%  ggplot(., aes (x = Drainage_A)) +
   ylab("Counts")
 
 
-#3.1A
-MeanVal <- function(x){
-  return (mean(x))
+#3
+
+
+InputVal <- function(x){
+  
+   if (is.numeric(x) == TRUE){
+   calStat <- list(mean(x), median(x),max(x),min(x))
+   SortVec <- sort(x)
+   ans <- list(calStat,SortVec)
+   return(ans)  # It only can be return a single value
+   } else {"Error... this input is not numeric"}
 }
-
-MedianVal<- function(x){
-  return (median(x))
-}
-
-MaxVal<- function(x){
-  return (max(x))
-}
-
-MinVal<- function(x){
-  return (min(x))
-}
-
-
-#3.1B
-
-SortVec <- function(x){
-  return (sort(x))
-}
-
-#3.1C
-
-SortA_list_B_sort <- function(a,b){
-  list_a <- as.list(a)
-  return(c(list_a, sort(b))) 
-}
-
-#3.1D
-testNumVal <- function(a){
-    ifelse(is.numeric(a),"True", "Error... this input is not numeric")
-  }
-
-a <- c(1,0,-1)
-b <- c(10,100,9,1000)
-SortA_list_B_sort(a,b)
-SortVec(b)
-tst = c("a","b","c")
-
 
 #4.1
 
@@ -107,14 +74,29 @@ tst = c("a","b","c")
 d.counties %>% sf::st_crs()
 d.stations %>% sf::st_crs()                                                             
 d.counties %>% sf::st_crs() == d.stations %>% sf::st_crs()
-
-del.counties2 <- d.counties 
-del.stations2 <- d.stations 
-del.stations_all <- sf::st_intersection(d.stations,d.counties)
-
+#---------------------------------------------------
+d.countiesdata <- st_make_valid(d.counties)
+d.stationsdata <- d.stations 
+d.stations_all <- sf::st_intersection(d.stationsdata,d.countiesdata)
+d.stations_all %>% as_tibble() %>% count(STATEFP10)
+#-------------------------------------------------
 
 
 #4.2
 
-NYCountyArea <- del.counties36  %>% st_area()
+NYCountyArea <- d.counties %>% dplyr::filter(STATEFP10 == 36 ) %>% st_area()
 mean(NYCountyArea)
+
+
+#4.3
+
+maxdra <- d.stations_all %>% as_tibble() %>% group_by(STATEFP10)%>%summarize(result = mean(Drainage_A))
+which((maxdra$result)==max(maxdra$result)) %>% d.stations_all[.,] %>% dplyr::select(STATEFP10, STATION_NA)
+
+
+
+
+# Testing the Q&A 
+
+d.stations_all1 <- sf::st_intersection(d.stationsdata,d.countiesdata)
+d.stations_all2 <- sf::st_intersection(d.countiesdata,d.stationsdata)
